a06cf987f0a78a38b4bee7a74e8b5ca9fd06a012
rahatmushfiqabir <abir_rahat@yahoo.com>
2021-02-08 23:02:54 +0600

    final com

--- app.js
+++ app.js
@@ -1,42 +1,24 @@
-// fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+// fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=${search}')
 //     .then(res=>res.json())
 //     .then(data => console.log(data));
-
 document.getElementById("searchBtn").addEventListener("click", function () {
-
-    
     document.getElementById("mealElement").style.display = 'none';
-    showNotification("");
-
     const mealName = document.getElementById("mealName").value;
-    
-    if (mealName == "") {
-        showNotification("Please Enter a meal name.")
-    } else {
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
-            .then(res => res.json())
-            .then(data => {
-                if (data.meals == null) {
-                    showNotification(`No meal found Please try again.`)
-                } else {
-                    searchMeals(data.meals);
-                }
-            })
-    }
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+        .then(res => res.json())
+        .then(data => {
+            if (data.meals == null) {
+                getNotification(`Sorry No meal found Please try again.`)
+            } else {
+                searchMeals(data.meals);
+            }
+        })
+
     document.getElementById("mealName").value = "";
+    getNotification("");
 })
-
-// check
-document.getElementById("mealName").addEventListener("keyup", event => {
-    if (event.key === "Enter") document.getElementById("searchBtn").click();
-});
-
-function showNotification(notificationText) {
-    document.getElementById("notification").innerText = notificationText;
-}
-
-
 function searchMeals(meals) {
+    document.getElementById("mealList").innerHTML = "";
     meals.forEach(meal => {
         console.log(meals);
         const mealDiv = document.createElement("detail");
@@ -44,16 +26,25 @@ function searchMeals(meals) {
         <div onclick='mealDetails("${meal.idMeal}")' class="mealPlate">
             <img src="${meal.strMealThumb}" class="mealImage">
             <h5 class="title text-center">${meal.strMeal}</h5>
+            <button id='ing-btn' class="btn btn-success ">check Ingredients</button>
         </div>
         `
         document.getElementById("mealList").appendChild(mealDiv);
     });
 }
 
+// check
+function getNotification(notificationText) {
+    document.getElementById("notification").innerText = notificationText;
+}
+document.getElementById("mealName").addEventListener("keyup", event => {
+    if (event.key === "Enter") document.getElementById("searchBtn").click();
+});
+
 // Clicked Item details
-function mealDetails(mealId) {
-    console.log(mealId);
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+function mealDetails(search) {
+    console.log(search);
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${search}`)
         .then(res => res.json())
         .then(data => {
             displayMealDetails(data.meals[0]);
@@ -66,27 +57,29 @@ function displayMealDetails(meal) {
         <h3 class="mealSingleTitle">${meal.strMeal}</h3>
     </div>
     <div>
-        <h4>  Ingredients</h4>
-         <ul id="ingredientList">
+        <h4 class="space">Ingredients</h4>
+         <ul class="space-list" id="ingredientList">
         </ul>
     </div>
     `;
 
-   
-
     //  Ingredients.
-    for (let i = 1; i <= 50; i++) {
-        let measure = 'strMeasure' + i;
+    let i =0;
+    while (i<15){
         let ingredient = 'strIngredient' + i;
-        if (meal[ingredient] === "" || meal[ingredient] == null) {
+        if (meal[ingredient] === "") {
             break;
         }
-
-        const li = document.createElement("li");
-        li.innerHTML = `
-        <li><i class="icon-color fas fa-check-square"></i> ${meal[measure]} ${meal[ingredient]}</li>
+        else{
+        const list = document.createElement("li");
+        list.innerHTML = `
+            <li class="list"><i class="icon-color fas fa-check-square"></i>
+            ${meal[ingredient]}
+            </li>
         `;
-        document.getElementById("ingredientList").appendChild(li)
+        document.getElementById("ingredientList").appendChild(list)
+        }
+        i++;
     }
 
     document.getElementById("mealElement").style.display = "block";
